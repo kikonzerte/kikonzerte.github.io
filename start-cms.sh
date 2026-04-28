a75c81de6b3e3af77b4c5c1e36c41516a056f840
PeterHeliko <spamalarm@outlook.com>
2026-04-28 20:27:18 +0200

    cms starting update

--- start-cms.sh
+++ start-cms.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-cd "$(dirname "$(readlink -f "$0")")"
-node cms-server.js
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+gnome-terminal -- bash -c "
+  [ -f ~/.nvm/nvm.sh ] && source ~/.nvm/nvm.sh
+  [ -f ~/.profile ] && source ~/.profile
+  [ -f ~/.bashrc ] && source ~/.bashrc
+  cd '$SCRIPT_DIR'
+  node cms-server.js
+  echo ''
+  echo 'Server stopped. Press Enter to close.'
+  read
+"
